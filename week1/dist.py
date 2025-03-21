4ebbfe4b60989008bba2075cef81da25b200763d
Ranadheer318 <ranadheer3108@gmail.com>
2025-03-21 16:14:01 +0530

    dist.py

---
+++ week1/dist.py
@@ -0,0 +1,6 @@
+x1=int(input('enter the value of x1'))
+x2=int(input('enter the  value of x2'))
+y1=int(input('enter the value of y1'))
+y2=int(input('enter the value of y2'))
+d=((x1-x2)**2+(y1-y2)**2)**1/2
+print("Distance is",d)
