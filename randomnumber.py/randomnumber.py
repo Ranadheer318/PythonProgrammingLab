dfddc1921526a15677b9b311c1627e1aada1ea11
Ranadheer318 <ranadheer3108@gmail.com>
2025-03-27 04:48:26 +0000

    randomnumber.py

---
+++ randomnumber.py/randomnumber.py
@@ -0,0 +1,12 @@
+import random
+n=random.randint(1,100)
+print("guess the number")
+x=int(input("guess the number"))
+while True:
+    if(x<n):
+      print("number is too low")
+    elif(x>n):
+      print("number is too high")
+    else:
+      print("congrats,you have found the number")
+
