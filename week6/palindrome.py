67dec3dfcf17bc44b705734d9c0262f4134c6d32
Ranadheer318 <ranadheer3108@gmail.com>
2025-05-02 10:44:32 +0530

    Create palindrome.py

---
+++ week6/palindrome.py
@@ -0,0 +1,14 @@
+a='BOB'
+if a[::]== a[::-1]:
+    print("Number is a PALINDROME")
+else:
+    print('Number is not a PALINDROME')
+
+
+
+
+a=[1,2,2,3,3,2,2,1]
+if a[::]== a[::-1]:
+    print("Number is a PALINDROME")
+else:
+    print('Number is not a PALINDROME')
